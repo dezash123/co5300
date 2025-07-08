8a6d7a847ce444219948553e6db07e13363843aa
dezash123 <desmondmehta@gmail.com>
2025-07-08 01:27:40 -0400

    add id read

--- src/lib.rs
+++ src/lib.rs
@@ -127,6 +127,13 @@ impl<SPI, TE, RST, SE, PE> Co5300<SPI, TE, RST>
         Timer::after_millis(100).await;
         self.spi_write(&[0x02, 0x00, 0x29, 0x00]).await?;
         info!("disp on");
+        
+        self.spi_write(&[0x03, 0x00, 0x0F, 0x00]).await?;
+        let mut buf = [0; 3];
+        self.spi.read(&mut buf).await.map_err(Error::SpiError)?;
+        info!("ID: {:?}", buf);
+
+
         Ok(self)
     }
 
